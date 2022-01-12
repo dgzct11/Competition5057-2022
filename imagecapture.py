4eb8b12f8890a335b81b35eedfae4981ab5ed495
dgzct11 <60042905+dgzct11@users.noreply.github.com>
2022-01-12 15:01:03 -0600

    Update imagecapture.py

--- imagecapture.py
+++ imagecapture.py
@@ -12,9 +12,11 @@ while (True):
 
     cv2.imshow('Result', frame)
     frame = np.copy(frame)
-    if cv2.waitKey(20) & 0xFF == ord('q'):
+    if cv2.waitKey(20) & 0xFF == ord('t'):
 
         cv2.imwrite(f"./images\\img_{i}.jpg", frame)
         i+=1
+    elif cv2.waitKey(20) & 0xFF == ord('q'):
+        break
 cv2.destroyAllWindows()
 
