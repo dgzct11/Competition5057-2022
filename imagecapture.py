e5a0059e93156677c38fb6e34a848fcb45b08b34
dgzct11 <60042905+dgzct11@users.noreply.github.com>
2022-01-12 15:10:45 -0600

    added labeling UI and image capture

--- imagecapture.py
+++ imagecapture.py
@@ -14,7 +14,7 @@ while (True):
     frame = np.copy(frame)
     if cv2.waitKey(20) & 0xFF == ord('t'):
 
-        cv2.imwrite(f"./images\\img_{i}.jpg", frame)
+        cv2.imwrite(f"images\\img_{i}.jpg", frame)
         i+=1
     elif cv2.waitKey(20) & 0xFF == ord('q'):
         break
